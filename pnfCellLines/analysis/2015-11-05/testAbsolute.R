0fda5d4e46eb63e37ebe83055d066c0daea14f44
Sara JC Gosline <sara.gosline@sagebase.org>
2015-11-06 13:13:50 -0800

    updated parameters again

--- pnfCellLines/analysis/2015-11-05/testAbsolute.R
+++ pnfCellLines/analysis/2015-11-05/testAbsolute.R
@@ -17,14 +17,14 @@ run_abs<-function(fname){
     max.sigma.h <- 0.02
     min.ploidy <- 0.90
     max.ploidy <- 8
-    max.as.seg.count <- 2500
-    max.non.clonal <- .2
-    max.neg.genome <- .2
-    copy_num_type <- "allelic"
+    max.as.seg.count <- 5000
+    max.non.clonal <- .4
+    max.neg.genome <- .4
+    copy_num_type <- "total"
 
     oname=gsub('.seg','_PNF_CellLineSample',fname)
     sname=paste(annots[match(gsub('.seg','',fname),annots$sample),c(5,3)],collapse=' ')
-    ##this doesn't run, file is off. let's try running hapseg instead
+
     RunAbsolute(fname,sample.name=sname,,min.ploidy=min.ploidy,max.ploidy=max.ploidy,
                 max.sigma.h=max.sigma.h,platform='SNP_6.0',copy_num_type='total',
                 min.mut.af=0,sigma.p=sigma.p,results.dir='abs_res',verbose=TRUE,
