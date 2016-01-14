70d7a5f6ef65cad1059f22e46fe6f9eb57ba2c45
Ubuntu <ubuntu@ip-172-31-55-243.ec2.internal>
2016-01-14 17:51:29 +0000

    testing sleuth

---
+++ pnfCellLines/analysis/2016-01-13/annotateUPloadH5s.R
@@ -0,0 +1,47 @@
+###get all RNA-Seq files from belltown directory where kallisto was run
+##map ENS transcripts to gene id
+##create new tab-delimited file with transcript AND gene id
+##annotate with appropriate cell line and upload
+
+
+
+require(synapseClient)
+library(data.table)
+synapseLogin()
+belltown.dir='../2016-01-13/'
+filedirs=list.files(belltown.dir)
+fqc=c(grep('fastqc',filedirs),grep('.out',filedirs))
+if(length(fqc)>0)
+    filedirs=filedirs[-fqc]
+                                        #each directory is a different lane/run.
+filedirs=filedirs[grep("C85",filedirs)]
+all.dat<-lapply(filedirs,function(x) paste(belltown.dir,x,'abundance.h5',sep='/'))
+names(all.dat)<-filedirs
+
+
+##first get all gene names, add to files
+
+
+##then create annotations
+samp.mapping=read.table(synGet('syn5562006')@filePath,sep=',',header=T)
+
+tab.q=synTableQuery('SELECT distinct "Sample Name","Sample ID","Sample Origin","Sample Genotype" FROM syn5014742')@values
+
+samp.to.cell<-lapply(filedirs,function(x){
+  cellLine=samp.mapping$CellLine[match(paste(x,'0',sep='_'),samp.mapping$File_Name)]
+  res=c(tab.q[match(cellLine,tab.q$`Sample Name`),],FlowCellLane=x)
+  names(res)<-c('sampleName','sampleID','sampleOrigin','sampleGenotype','flowCellLane')
+  res
+})
+names(samp.to.cell)=filedirs
+
+##write files
+file.res<-lapply(filedirs,function(x){
+  filename=paste(x,'_RNASeq_Kallisto_gencodev24_quants.h5',sep='')
+  file.copy(all.dat[[x]],filename) 
+ #write.table(newfiles[[x]],file=filename,sep='\t',row.names=F)
+  newf=File(path=filename,parentId='syn5579785')
+  synSetAnnotations(newf)<-samp.to.cell[[x]]
+  newf=synStore(newf,executed='https://raw.githubusercontent.com/Sage-Bionetworks/NTAP/master/pnfCellLines/analysis/2015-01-13/annotateUPloadH5s.R')
+})
+##store on synapse
---
+++ pnfCellLines/analysis/2016-01-14/testSleuth.R
@@ -0,0 +1,20 @@
+##now we want to test diffex
+library('sleuth')
+library('synapseClient')
+synapseLogin()
+
+h5files<-synapseQuery("select name,id from entity where parentId=='syn5579785'")
+h5files=h5files[grep('*h5',h5files[,1]),]
+
+h5s=lapply(h5files[,2],function(x) synGet(x))
+snames=lapply(h5s,function(x) x@annotations$sampleName)
+sgens=lapply(h5s,function(x) x@annotations$sampleGenotype)
+filepaths=lapply(h5s,function(x) x@filePath)
+
+##build the contrast matrix from the annotations
+df=data.frame(sample=unlist(snames),path=unlist(filepaths),Genotype=unlist(sgens))
+df$path=as.character(df$path)
+so<-sleuth_prep(df,~ Genotype)
+write.table(summary(so),file='kal_quant_summary.tsv',sep='\t')
+so<-sleuth_fit(so)
+so <- sleuth_wt(so, 'Genotype++')
