41496019b8d1089e9c6ff6c9c08ff3e153c19780
Sara JC Gosline <sara.gosline@sagebase.org>
2016-01-24 20:04:40 -0800

    updated

--- pnfCellLines/analysis/2016-01-24/evalCNV.R
+++ pnfCellLines/analysis/2016-01-24/evalCNV.R
@@ -8,7 +8,7 @@ midthresh=idDiffVals(segdata2,thresh=-0.5)
 midthresh=idDiffVals(segdata2,thresh=-0.5,byChrom=T)
 
 afiles=list.files('./')
-sapply(afiles[grep('.png',afiles)] function(x){
+sapply(afiles[grep('.png',afiles)], function(x){
   nf=File(x,parentId='syn5605333')
-  synStore(nf,executed=list(list(url=''),list(url="")))
+  synStore(nf,executed=list(list(url='https://raw.githubusercontent.com/Sage-Bionetworks/NTAP/master/pnfCellLines/bin/clusterCNVBySample.R'),list(url="https://raw.githubusercontent.com/Sage-Bionetworks/NTAP/master/pnfCellLines/analysis/2016-01-24/evalCNV.R")))
 })
