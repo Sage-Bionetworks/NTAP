b82731de901134d6eeff3d762d91104eba4cca10
Sara JC Gosline <sara.gosline@sagebase.org>
2016-02-22 17:16:07 -0800

    Updated script to run over night :w# Please enter the commit message for your changes. Lines starting

--- pnfCellLines/analysis/2016-02-22/testRnaDrugNormalization.R
+++ pnfCellLines/analysis/2016-02-22/testRnaDrugNormalization.R
@@ -3,6 +3,8 @@ source("../../bin/crossDataComps.R")
 
 alpha.pars=c(1,4,5,10,100,1000)
 
+this.script='https://raw.githubusercontent.com/Sage-Bionetworks/NTAP/master/pnfCellLines/analysis/2016-02-22/testRnaDrugNormalization.R'
+
 ###build functions to identify ideal ALPHA parameter for transform
 #' find best alpha parameter in CTD2/CCLE data
 #' @param alpha.params - list of parameters to sample
@@ -15,10 +17,10 @@ testCTD2Norm<-function(){
   ccle.tpm<-getCCLEDataTPM(removeDupes=TRUE)
 
   for(ap in alpha.pars){
-    res=computeDrugRnaNormalizedCor(drugMat=ctrpMat,rnaMat=ccle.tpm,prefix='ctrpOriginal',sampleCombos=10000,alpha=5)
+    res=computeDrugRnaNormalizedCor(drugMat=ctrpMat,rnaMat=ccle.tpm,prefix='ctrpOriginal',sampleCombos=NA,alpha=ap)
     for(f in res){
       sf=File(f,parentId='syn5679539')
-      synapseStore(sf,used=list())
+      synapseStore(sf,used=list(url=this.script,wasExecuted=TRUE))
     }
       
   }
@@ -31,8 +33,19 @@ testCTD2Norm<-function(){
 #' @return file names to upload to synapse
 testNcatsNorm<-function(){
   ##ncats vs. cell lines
+  source('../../bin/RNASeqData.R')
+  source("../../bin/ncatsSingleAgentScreens.R")
   genCodeMat<-rnaGencodeKallistoMatrix(useCellNames=TRUE)
   drugMat<-getValueForAllCells("FAUC")
+  
+  for(ap in alpha.pars){
+    res=computeDrugRnaNormalizedCor(drugMat=drugMat,rnaMat=genCodeMat,prefix='ncatsOriginal',sampleCombos=NA,alpha=ap)
+    for(f in res){
+      sf=File(f,parentId='syn5679539')
+      synapseStore(sf,used=list(url=this.script,wasExecuted=TRUE))
+    }
+    
+  }
 }
 
 
