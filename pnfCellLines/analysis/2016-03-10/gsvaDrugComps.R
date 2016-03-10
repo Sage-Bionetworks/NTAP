37f0f00fc6a5172f365012a874a2dd18d0b98fef
Sara JC Gosline <sgosline@mit.edu>
2016-03-10 14:22:24 -0800

    Updated drug comparison

--- pnfCellLines/analysis/2016-03-10/gsvaDrugComps.R
+++ pnfCellLines/analysis/2016-03-10/gsvaDrugComps.R
@@ -9,7 +9,18 @@ gsva=as.matrix(read.table(synGet('syn5689231')@filePath))
 colnames(gsva)[1]='ipNF05.5 (mixed clone)'
 colnames(gsva)[11]='ipNF05.5 (single clone)'
 
-drug.by.cell=acast(getRecalculatedAUCTab(),Cell~Drug,value.var='AUC',fun.aggregate=function(x) mean(x,na.rm=T))
+gsva.pvals=as.matrix(read.table(synGet('syn5689235')@filePath))
+sig.enrich=which(apply(gsva.pvals,1,mean)<0.1)
+gsva=gsva[sig.enrich,]
+
+maxr.vals=getValueForAllCells("MAXR")
+maxr.range<-apply(maxr.vals,1,function(x) max(x,na.rm=T)-min(x,na.rm=T))
+
+auc.vals<-getValueForAllCells('FAUC')
+
+
+#drug.by.cell=acast(getRecalculatedAUCTab(),Cell~Drug,value.var='AUC',fun.aggregate=function(x) mean(x,na.rm=T))
+drug.by.cell <- t(auc.vals[names(sort(maxr.range,decreasing=T))[1:200],])
 
 gsva.cells=intersect(colnames(gsva),rownames(drug.by.cell))
 gsva.drugcor=cor(t(gsva[,gsva.cells]),drug.by.cell[gsva.cells,])
@@ -18,8 +29,8 @@ gsva.drugcor=cor(t(gsva[,gsva.cells]),drug.by.cell[gsva.cells,])
 allTops<-sapply(colnames(gsva.drugcor),function(x){
   ##first lets select by threshold
   tops<-names(which(abs(gsva.drugcor[,x])>0.95))
-  if(length(tops)>1)
-    tops=tops[which(apply(gsva[tops,],1,function(x) max(x,na.rm=T)-min(x,na.rm=T))>1.5)]
+#  if(length(tops)>1)
+#    tops=tops[which(apply(gsva[tops,],1,function(x) max(x,na.rm=T)-min(x,na.rm=T))>1.5)]
   if(length(tops)>12)
     tops<-names(sort(abs(gsva.drugcor[tops,x]),decreasing=T))[1:12]
   tops<-intersect(tops,rownames(gsva))
@@ -45,7 +56,7 @@ allPaths<-sapply(rownames(gsva.drugcor),function(z){
   #  tops=tops[which(apply(gsva[tops],1,function(x) max(x,na.rm=T)-min(x,na.rm=T))>1.5)]
   
   tops<-intersect(tops,colnames(drug.by.cell))
-  if(length(tops)<2)
+  if(length(tops)<1)
     return(NULL)
   restab<-do.call('rbind',lapply(tops,function(x) 
     data.frame(Drug=rep(x,length(gsva.cells)),Cell=gsva.cells,Enrichment=gsva[z,gsva.cells],AUC=drug.by.cell[gsva.cells,x])))
